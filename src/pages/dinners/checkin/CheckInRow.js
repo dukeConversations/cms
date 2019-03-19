9dda725a763f384e624e3d8224acc3d7d77aee63
Cooper Edmunds <cooper6789@gmail.com>
2019-03-19 12:04:02 -0400

    Make small changes to check-in

--- src/pages/dinners/checkin/CheckInRow.js
+++ src/pages/dinners/checkin/CheckInRow.js
@@ -9,17 +9,25 @@ export default class CheckInRow extends Component {
   render() {
     let application = this.props.application;
 
-    let buttonText = application.present ? "Mark as Absent" : "Mark as Present";
+    let checkInButton = application.present ? (
+      <Button color="danger" onClick={this.onClickAction}>
+        Mark as Absent
+      </Button>
+    ) : (
+      <Button color="success" onClick={this.onClickAction}>
+        Mark as Present
+      </Button>
+    );
     return (
       <tr>
         <td className="text-left align-middle">
           {application.student.firstName + " " + application.student.lastName}
+          <br />
+          <a href={"tel:" + application.student.phoneNumber}>
+            {application.student.phoneNumber}
+          </a>
         </td>
-        <td className="text-right align-middle">
-          <Button backgroundColor="red" onClick={this.onClickAction}>
-            {buttonText}
-          </Button>
-        </td>
+        <td className="text-right align-middle">{checkInButton}</td>
       </tr>
     );
   }
